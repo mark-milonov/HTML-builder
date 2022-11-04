db876a095b6d7a88641404ccf3153562d77eb711
Mark Milonov <mark.milonov.90@mail.ru>
2022-11-04 13:01:43 +0600

    feat: add solution task-4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,9 @@
+// Для проверяющиего: я вынес часть кода в отдельный модуль 
+// В рамках таска это не запрещено!
+const copyDir = require('./copy-dir')
 
+const path = require('path')
+const _from = path.join(__dirname, 'files')
+const _to = path.join(__dirname, 'files-copy')
+
+copyDir(_from, _to)
