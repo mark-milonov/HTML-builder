d30070eeae831f42cbfc8ec019f6415ac99b37b8
Mark Milonov <mark.milonov.90@mail.ru>
2022-11-05 21:12:53 +0600

    refactor: contain all code in index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,34 @@
-// Для проверяющиего: я вынес часть кода в отдельный модуль 
-// В рамках таска это не запрещено!
-const copyDir = require('./copy-dir')
+// Хотел вынести функцию в отдельный файл - сказали нельзя
+// const copyDir = require('./copy-dir')
 
 const path = require('path')
 const _from = path.join(__dirname, 'files')
 const _to = path.join(__dirname, 'files-copy')
 
 copyDir(_from, _to)
+
+async function copyDir(fromPath, toPath) {
+  const fs = require('fs');
+  const fsProm = require('fs/promises')
+  const path = require('path');
+
+  await fsProm.rm(toPath, {recursive: true, force: true})
+  await fsProm.mkdir(toPath, {recursive: true})
+
+  fs.readdir(fromPath, {withFileTypes: true}, (err, files) => {
+    if(err) return console.log(err);
+    files.forEach(file => {
+      const fromFile = path.join(fromPath, file.name)
+      const toFile = path.join(toPath, file.name)
+      if(file.isFile()) {
+        const ws = fs.createWriteStream(toFile);
+        const rs = fs.createReadStream(fromFile);
+        rs.pipe(ws)
+          .on('error', err => console.log(err));
+      }
+      if(file.isDirectory()) {
+        copyDir(fromFile, toFile)
+      }
+    })
+  })
+}
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,9 +1,25 @@
-// Для проверяющиего: я вынес часть кода в отдельный модуль 
-// В рамках таска это не запрещено!
-const bundleFile = require('./style-bundler');
+// Хотел вынести функцию в отдельный файл - сказали нельзя
+// const bundleFile = require('./style-bundler');
 
 const path = require('path');
 const styles = path.join(__dirname, 'styles');
 const dist = path.join(__dirname, 'project-dist', 'bundle.css');
 
 bundleFile(styles, dist);
+
+function bundleFile(fromFolder, toFile) {
+  const fs = require('fs');
+  const path = require('path');
+  const ws = fs.createWriteStream(toFile, 'utf-8');
+  fs.readdir(fromFolder, {withFileTypes: true, encoding: 'utf-8'}, (err, files) => {
+    if(err) return console.log(err);
+    files
+      .filter(file => file.isFile())
+      .filter(file => path.parse(file.name).ext == '.css')
+      .forEach(file => {
+        const filePath = path.join(fromFolder, file.name);
+        const rs = fs.createReadStream(filePath);
+        rs.pipe(ws);
+      })
+  });
+}
